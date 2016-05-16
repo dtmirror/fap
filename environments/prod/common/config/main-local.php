35469fc3b052ad105db51dc8ce0844fef7c61826
Armand Niculescu <armand.niculescu@gmail.com>
2016-05-16 21:40:17 +0300

    Initial draft

---
+++ environments/prod/common/config/main-local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=fap',
+            'username' => 'root',
+            'password' => 'r00t',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
